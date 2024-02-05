40dcbc91b1a426270f04e487aff2f6341aa21512
Marlon <marlonfontanies@gmail.com>
2024-02-05 02:22:29 -0500

    fixing seed 2

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -10,12 +10,14 @@ import { Tag } from './entities/product-tags.entity';
 import { ProductsCombinations } from './entities/product-combinations.entity';
 import { Venta } from 'src/ventas/entities/venta.entity';
 import { TagImage } from './entities/tag-images.entity';
+import { Anounce } from 'src/anuncios/entities/anuncio.entity';
+import { AnounceMedia } from 'src/anuncios/entities/anounce-media.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   imports:[
-    TypeOrmModule.forFeature([ Product, ProductImage, Tag, ProductsCombinations, Venta, TagImage]),
+    TypeOrmModule.forFeature([ Product, ProductImage, Tag, ProductsCombinations, Venta, TagImage, Anounce, AnounceMedia]),
     AuthModule,
   ],
   exports:[
